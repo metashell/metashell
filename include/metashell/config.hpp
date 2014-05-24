5c5cbf3f3d56c84a15186187979cdd01265dc073
Abel Sinkovics <abelsoft@freemail.hu>
2014-05-24 14:35:14 +0200

    Disabled precompiled header usage by default when clang is not found

--- include/metashell/config.hpp
+++ include/metashell/config.hpp
@@ -53,7 +53,7 @@ namespace metashell
       indent(true),
       standard_to_use(standard::cpp11),
       warnings_enabled(true),
-      use_precompiled_headers(true),
+      use_precompiled_headers(!clang_path_.empty()),
       clang_path(clang_path_)
     {}
   };
