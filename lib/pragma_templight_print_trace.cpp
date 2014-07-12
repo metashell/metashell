f3c3e8d01fde19708817c8f84a03a9cd2e430838
Andras Kucsma <r0maikx02b@gmail.com>
2014-07-12 15:45:26 +0200

    Add new empty print trace pragma

---
+++ lib/pragma_templight_print_trace.cpp
@@ -0,0 +1,32 @@
+
+#include <metashell/pragma_templight_print_trace.hpp>
+#include <metashell/shell.hpp>
+
+using namespace metashell;
+
+pragma_templight_print_trace::pragma_templight_print_trace(shell& shell_) :
+  _shell(shell_)
+{}
+
+pragma_handler_interface* pragma_templight_print_trace::clone() const
+{
+  return new pragma_templight_print_trace(_shell);
+}
+
+std::string pragma_templight_print_trace::arguments() const
+{
+  return "";
+}
+
+std::string pragma_templight_print_trace::description() const
+{
+  return "Prints the last template stack trace templight produced";
+}
+
+void pragma_templight_print_trace::run(
+  const token_iterator& args_begin_,
+  const token_iterator& args_end_
+) const
+{
+}
+
