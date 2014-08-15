1ccd32d368e5e83bfacd6625fe31cd8589a88eff
Andras Kucsma <r0maikx02b@gmail.com>
2014-08-15 13:10:35 +0200

    Add test for instantiation_kind

---
+++ test/test_instantiation_kind.cpp
@@ -0,0 +1,54 @@
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2014, Andras Kucsma (andras.kucsma@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <metashell/instantiation_kind.hpp>
+
+#include <just/test.hpp>
+
+#include <string>
+#include <sstream>
+
+
+using namespace metashell;
+
+std::string to_string(instantiation_kind kind) {
+  std::stringstream ss;
+  ss << kind;
+  return ss.str();
+}
+
+JUST_TEST_CASE(test_instantiation_kind_print)
+{
+  JUST_ASSERT_EQUAL(to_string(template_instantiation),
+      "TemplateInstantiation");
+  JUST_ASSERT_EQUAL(to_string(default_template_argument_instantiation),
+      "DefaultTemplateArgumentInstantiation");
+  JUST_ASSERT_EQUAL(to_string(default_function_argument_instantiation),
+      "DefaultFunctionArgumentInstantiation");
+  JUST_ASSERT_EQUAL(to_string(explicit_template_argument_substitution),
+      "ExplicitTemplateArgumentSubstitution");
+  JUST_ASSERT_EQUAL(to_string(deduced_template_argument_substitution),
+      "DeducedTemplateArgumentSubstitution");
+  JUST_ASSERT_EQUAL(to_string(prior_template_argument_substitution),
+      "PriorTemplateArgumentSubstitution");
+  JUST_ASSERT_EQUAL(to_string(default_template_argument_checking),
+      "DefaultTemplateArgumentChecking");
+  JUST_ASSERT_EQUAL(to_string(exception_spec_instantiation),
+      "ExceptionSpecInstantiation");
+  JUST_ASSERT_EQUAL(to_string(memoization),
+      "Memoization");
+}
+
