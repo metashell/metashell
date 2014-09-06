a95ace0a02c225533b8414f9c6c727ea747e561e
Andras Kucsma <r0maikx02b@gmail.com>
2014-09-06 17:13:58 +0200

    Add new tests for mdb step

--- test/test_metadebugger_step.cpp
+++ test/test_metadebugger_step.cpp
@@ -32,3 +32,89 @@ JUST_TEST_CASE(test_mdb_step_without_evaluation) {
       "Metaprogram finished or not evaluated yet\n");
 }
 #endif
+
+#ifndef METASHELL_DISABLE_TEMPLIGHT_TESTS
+JUST_TEST_CASE(test_mdb_step_fibonacci) {
+  metadebugger_test_shell sh =
+    metadebugger_test_shell::create_with_environment(fibonacci_mp);
+
+  sh.line_available("evaluate int_<fib<10>::value>");
+
+  sh.clear_output();
+  sh.line_available("step");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<10> (TemplateInstantiation)\n");
+}
+#endif
+
+#ifndef METASHELL_DISABLE_TEMPLIGHT_TESTS
+JUST_TEST_CASE(test_mdb_step_2_fibonacci) {
+  metadebugger_test_shell sh =
+    metadebugger_test_shell::create_with_environment(fibonacci_mp);
+
+  sh.line_available("evaluate int_<fib<10>::value>");
+
+  sh.clear_output();
+  sh.line_available("step 2");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<8> (TemplateInstantiation)\n");
+}
+#endif
+
+#ifndef METASHELL_DISABLE_TEMPLIGHT_TESTS
+JUST_TEST_CASE(test_mdb_step_fibonacci_twice) {
+  metadebugger_test_shell sh =
+    metadebugger_test_shell::create_with_environment(fibonacci_mp);
+
+  sh.line_available("evaluate int_<fib<10>::value>");
+
+  sh.clear_output();
+  sh.line_available("step");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<10> (TemplateInstantiation)\n");
+
+  sh.clear_output();
+  sh.line_available("step");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<8> (TemplateInstantiation)\n");
+}
+#endif
+
+#ifndef METASHELL_DISABLE_TEMPLIGHT_TESTS
+JUST_TEST_CASE(test_mdb_step_0_fibonacci_at_start) {
+  metadebugger_test_shell sh =
+    metadebugger_test_shell::create_with_environment(fibonacci_mp);
+
+  sh.line_available("evaluate int_<fib<10>::value>");
+
+  sh.clear_output();
+  sh.line_available("step 0");
+
+  JUST_ASSERT_EQUAL(sh.get_output(), "");
+}
+#endif
+
+#ifndef METASHELL_DISABLE_TEMPLIGHT_TESTS
+JUST_TEST_CASE(test_mdb_step_0_fibonacci_after_step) {
+  metadebugger_test_shell sh =
+    metadebugger_test_shell::create_with_environment(fibonacci_mp);
+
+  sh.line_available("evaluate int_<fib<10>::value>");
+
+  sh.clear_output();
+  sh.line_available("step");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<10> (TemplateInstantiation)\n");
+
+  sh.clear_output();
+  sh.line_available("step 0");
+
+  JUST_ASSERT_EQUAL(sh.get_output(),
+      "fib<10> (TemplateInstantiation)\n");
+}
+#endif
