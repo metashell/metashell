f159a3e7f8f421917004d9c8ed2d42c64d0105f1
Abel Sinkovics <abelsoft@freemail.hu>
2014-09-09 22:29:10 +0200

    Fix Windows build

--- lib/readline_environment.cpp
+++ lib/readline_environment.cpp
@@ -23,6 +23,8 @@
 #  include <sys/ioctl.h>
 #endif
 
+#include <vector>
+
 namespace metashell {
 
 readline_environment::readline_environment() :
@@ -68,7 +70,8 @@ std::string readline_environment::get_edited_text() {
 
 void readline_environment::add_history(const std::string& line) {
   //TODO save/restore history
-  ::add_history(line.c_str());
+  std::vector<char> l(line.c_str(), line.c_str() + line.size() + 1);
+  ::add_history(l.data());
 }
 
 void readline_environment::set_rl_attempted_completion_function(
