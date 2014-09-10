8cdc37d69a19ad5b84419c0e9b7827056f46973d
Andras Kucsma <r0maikx02b@gmail.com>
2014-09-11 00:01:07 +0200

    Add highlight_syntax

---
+++ lib/highlight_syntax.cpp
@@ -0,0 +1,56 @@
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2014, Andras Kucsma (andras.kucsma@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <metashell/command.hpp>
+
+#include <metashell/highlight_syntax.hpp>
+
+namespace metashell {
+
+colored_string::color_t color_of_token(const token& t_) {
+  using just::console::color;
+
+  switch (t_.category())
+  {
+  case token_category::character_literal:
+  case token_category::floating_literal:
+  case token_category::integer_literal:
+  case token_category::string_literal:
+  case token_category::bool_literal:
+  case token_category::preprocessor:
+    return color::magenta;
+  case token_category::keyword:
+    return color::bright_green;
+  case token_category::comment:
+    return color::green;
+  default:
+    return boost::none;
+  }
+}
+
+colored_string highlight_syntax(const std::string& str) {
+  colored_string result;
+
+  const command cmd(str);
+  for (const token& t : cmd) {
+    result += colored_string(t.value(), color_of_token(t));
+  }
+
+  return result;
+}
+
+}
+
