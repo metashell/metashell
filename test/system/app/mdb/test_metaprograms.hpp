03e478f6df003b742b924c0ed6df7f0579eda5b0
Andras Kucsma <r0maikx02b@gmail.com>
2015-02-07 00:00:08 +0100

    Move mdb next tests to system tests

---
+++ test/system/app/mdb/test_metaprograms.hpp
@@ -0,0 +1,70 @@
+#ifndef METASHELL_TEST_METAPROGRAMS_HPP
+#define METASHELL_TEST_METAPROGRAMS_HPP
+
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2014, Andras Kucsma (andras.kucsma@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version; 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <string>
+
+const std::string fibonacci_mp =
+  "template <int N> struct fib {"
+    "static constexpr int value = fib<N - 2>::value + fib<N - 1>::value;"
+  "};"
+  "template <> struct fib<0> { static constexpr int value = 0; };"
+  "template <> struct fib<1> { static constexpr int value = 1; };"
+  "template <int N> struct int_ {};";
+
+const std::string missing_value_fibonacci_mp =
+  "template <int N> struct fib {"
+    "static constexpr int value = fib<N - 2>::value + fib<N - 1>::value;"
+  "};"
+  "template <> struct fib<0> {};"
+  "template <> struct fib<1> { static constexpr int value = 1; };"
+  "template <int N> struct int_ {};";
+
+const std::string fibonacci_with_enum_mp =
+  "template <int N> struct fib {"
+    "enum ENUM { value = fib<N - 2>::value + fib<N - 1>::value };"
+  "};"
+  "template <> struct fib<0> { enum ENUM { value = 0 }; };"
+  "template <> struct fib<1> { enum ENUM { value = 1 }; };"
+  "template <int N> struct int_ {};";
+
+const std::string multi_fibonacci_mp =
+  "template <int N>"
+  "struct multi_fib {"
+    "static constexpr int value ="
+        "multi_fib<N - 4>::value +"
+        "multi_fib<N - 2>::value +"
+        "multi_fib<N - 3>::value +"
+        "multi_fib<N - 1>::value;"
+  "};"
+  "template <> struct multi_fib<0> { static constexpr int value = 1; };"
+  "template <> struct multi_fib<1> { static constexpr int value = 1; };"
+  "template <> struct multi_fib<2> { static constexpr int value = 1; };"
+  "template <> struct multi_fib<3> { static constexpr int value = 1; };"
+  "template <int N> struct int_ {};";
+
+const std::string template_specialization_mp =
+  "template<int N, int M> struct foo;"
+  "template<int N> struct foo<N, 1> {"
+    "static constexpr int value = 42 + N;"
+  "};"
+  "template <int N> struct int_ {};";
+
+#endif
+
+
---
+++ test/system/app/mdb/test_next.cpp
@@ -0,0 +1,78 @@
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2015, Andras Kucsma (andras.kucsma@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <metashell_system_test/type.hpp>
+#include <metashell_system_test/frame.hpp>
+#include <metashell_system_test/raw_text.hpp>
+#include <metashell_system_test/run_metashell.hpp>
+#include <metashell_system_test/json_generator.hpp>
+
+#include "test_metaprograms.hpp"
+
+#include <just/test.hpp>
+
+using namespace metashell_system_test;
+
+JUST_TEST_CASE(test_mdb_next_fib_from_root) {
+  const auto r =
+    run_metashell(
+      {
+        command(fibonacci_mp),
+        command("#msh mdb int_<fib<10>::value>"),
+        command("next")
+      }
+    );
+
+  auto i = r.begin() + 4;
+
+  JUST_ASSERT_EQUAL(raw_text("Metaprogram finished"), *i++);
+  JUST_ASSERT_EQUAL(type("int_<55>"), *i++);
+}
+
+JUST_TEST_CASE(test_mdb_next_minus_1_multi_fib_from_after_step) {
+  const auto r =
+    run_metashell(
+      {
+        command(multi_fibonacci_mp),
+        command("#msh mdb int_<multi_fib<10>::value>"),
+        command("step 4"),
+        command("next"),
+        command("next"),
+        command("next -1")
+      }
+    );
+
+  auto i = r.begin() + 4;
+  JUST_ASSERT_EQUAL(
+      frame(type("multi_fib<4>"), instantiation_kind::template_instantiation),
+      *i++);
+
+  ++i;
+  JUST_ASSERT_EQUAL(
+      frame(type("multi_fib<5>"), instantiation_kind::template_instantiation),
+      *i++);
+
+  ++i;
+  JUST_ASSERT_EQUAL(
+      frame(type("multi_fib<8>"), instantiation_kind::template_instantiation),
+      *i++);
+
+  ++i;
+  JUST_ASSERT_EQUAL(
+      frame(type("multi_fib<6>"), instantiation_kind::template_instantiation),
+      *i);
+}
+
