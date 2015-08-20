0aeac7471d4bf7b30c8d594a7b68ad4c5e82a7ec
Andras Kucsma <r0maikx02b@gmail.com>
2015-08-20 22:27:33 +0200

    Add profile info to data::frame

--- include/metashell/data/frame.hpp
+++ include/metashell/data/frame.hpp
@@ -33,23 +33,30 @@ namespace metashell
     public:
       frame() = default;
 
-      explicit frame(const type& name_);
+      explicit frame(const type& name_,
+        boost::optional<double> time_taken = boost::none);
 
       frame(const type& name_,
         const file_location& point_of_instantiation_,
-        instantiation_kind kind_);
+        instantiation_kind kind_,
+        boost::optional<double> time_taken = boost::none);
 
       const type& name() const;
 
       bool is_full() const;
+      bool is_profiled() const;
 
       // precondition: is_full()
       instantiation_kind kind() const;
       const file_location& point_of_instantiation() const;
+
+      // precondition: is_profiled()
+      double time_taken() const;
     private:
       type _name;
       boost::optional<file_location> _point_of_instantiation;
       boost::optional<data::instantiation_kind> _kind;
+      boost::optional<double> _time_taken;
     };
 
     std::ostream& operator<<(std::ostream& o_, const frame& f_);
