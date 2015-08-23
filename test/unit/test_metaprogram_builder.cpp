442021c0fe6efa3e4ad02610e14d02da7578c882
Andras Kucsma <r0maikx02b@gmail.com>
2015-08-23 13:01:09 +0200

    Add tests for metaprogram_builder

---
+++ test/unit/test_metaprogram_builder.cpp
@@ -0,0 +1,199 @@
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2015, Andras Kucsma (andras.kucsma@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <metashell/metaprogram_builder.hpp>
+
+#include <just/test.hpp>
+
+using namespace metashell;
+
+JUST_TEST_CASE(test_metaprogram_builder_normal_mode) {
+  metaprogram_builder mb(
+    metaprogram::mode_t::normal, "root_name", data::type("eval_result"));
+
+  mb.handle_template_begin(
+    data::instantiation_kind::template_instantiation,
+    "type<A>",
+    data::file_location("file", 10, 20),
+    100.0);
+
+  mb.handle_template_end(110.0);
+
+  metaprogram mp = mb.get_metaprogram();
+
+  JUST_ASSERT_EQUAL(metaprogram::mode_t::normal, mp.get_mode());
+  JUST_ASSERT_EQUAL(2u, mp.get_num_vertices());
+  JUST_ASSERT_EQUAL(1u, mp.get_num_edges());
+
+  JUST_ASSERT(mp.is_at_start());
+
+  mp.step();
+
+  auto frame = mp.get_current_frame();
+
+  JUST_ASSERT(frame.is_full());
+  JUST_ASSERT(!frame.is_profiled());
+  JUST_ASSERT_EQUAL("type<A>", frame.name().name());
+  JUST_ASSERT_EQUAL(
+    data::instantiation_kind::template_instantiation, frame.kind());
+
+  mp.step();
+
+  JUST_ASSERT(mp.is_finished());
+}
+
+JUST_TEST_CASE(test_metaprogram_builder_full_mode) {
+  metaprogram_builder mb(
+    metaprogram::mode_t::full, "root_name", data::type("eval_result"));
+
+  mb.handle_template_begin(
+    data::instantiation_kind::template_instantiation,
+    "type<A>",
+    data::file_location("file", 10, 20),
+    100.0);
+
+  mb.handle_template_begin(
+    data::instantiation_kind::template_instantiation,
+    "type<B>",
+    data::file_location("file", 20, 20),
+    110.0);
+
+  mb.handle_template_end(120.0);
+
+  mb.handle_template_end(130.0);
+
+  mb.handle_template_begin(
+    data::instantiation_kind::memoization,
+    "type<A>",
+    data::file_location("file", 10, 20),
+    140.0);
+
+  mb.handle_template_end(150.0);
+
+  metaprogram mp = mb.get_metaprogram();
+
+  JUST_ASSERT_EQUAL(metaprogram::mode_t::full, mp.get_mode());
+  JUST_ASSERT_EQUAL(3u, mp.get_num_vertices());
+  JUST_ASSERT_EQUAL(3u, mp.get_num_edges());
+
+  JUST_ASSERT(mp.is_at_start());
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(!frame.is_full());
+    JUST_ASSERT(!frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<A>", frame.name().name());
+  }
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(!frame.is_full());
+    JUST_ASSERT(!frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<B>", frame.name().name());
+  }
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(!frame.is_full());
+    JUST_ASSERT(!frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<A>", frame.name().name());
+  }
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(!frame.is_full());
+    JUST_ASSERT(!frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<B>", frame.name().name());
+  }
+
+  mp.step();
+
+  JUST_ASSERT(mp.is_finished());
+
+}
+
+JUST_TEST_CASE(test_metaprogram_builder_profile_mode) {
+  metaprogram_builder mb(
+    metaprogram::mode_t::profile, "root_name", data::type("eval_result"));
+
+  mb.handle_template_begin(
+    data::instantiation_kind::template_instantiation,
+    "type<A>",
+    data::file_location("file", 10, 20),
+    100.0);
+
+  mb.handle_template_end(110.0);
+
+  mb.handle_template_begin(
+    data::instantiation_kind::template_instantiation,
+    "type<B>",
+    data::file_location("file", 10, 20),
+    120.0);
+
+  mb.handle_template_end(140.0);
+
+  metaprogram mp = mb.get_metaprogram();
+
+  JUST_ASSERT_EQUAL(metaprogram::mode_t::profile, mp.get_mode());
+  JUST_ASSERT_EQUAL(3u, mp.get_num_vertices());
+  JUST_ASSERT_EQUAL(2u, mp.get_num_edges());
+
+  JUST_ASSERT(mp.is_at_start());
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(frame.is_full());
+    JUST_ASSERT(frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<B>", frame.name().name());
+    JUST_ASSERT_EQUAL(
+      data::instantiation_kind::template_instantiation, frame.kind());
+    JUST_ASSERT_EQUAL(20.0, frame.time_taken());
+    JUST_ASSERT_EQUAL(0.5, frame.time_taken_ratio());
+  }
+
+  mp.step();
+
+  {
+    auto frame = mp.get_current_frame();
+
+    JUST_ASSERT(frame.is_full());
+    JUST_ASSERT(frame.is_profiled());
+    JUST_ASSERT_EQUAL("type<A>", frame.name().name());
+    JUST_ASSERT_EQUAL(
+      data::instantiation_kind::template_instantiation, frame.kind());
+    JUST_ASSERT_EQUAL(10.0, frame.time_taken());
+    JUST_ASSERT_EQUAL(0.25, frame.time_taken_ratio());
+  }
+
+  mp.step();
+
+  JUST_ASSERT(mp.is_finished());
+}
