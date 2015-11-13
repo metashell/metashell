f5180138734415b5a2ae627f7571c6dd30cca927
Andras Kucsma <r0maikx02b@gmail.com>
2015-11-13 11:17:38 +0000

    Add set -e to build.sh

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+
 if [ ! -d cmake ]
 then
   echo "Please run this script from the root directory of the Metashell source code"
