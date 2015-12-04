7f80d195d9ee28bd8b6f86b4d58e59081de7a428
Andras Kucsma <andras.kucsma@prezi.com>
2015-12-04 23:23:32 +0100

    Run STs on travis on OS X

--- tools/travis_osx.sh
+++ tools/travis_osx.sh
@@ -2,11 +2,29 @@
 
 set -ex
 
+brew install wget
 if [ "$CXX" = "g++" ]; then
   brew update >/dev/null
   brew install homebrew/versions/gcc5
   export CXX="g++-5"
 fi
+
+# Test that the download version links are correct
+
+git fetch --tags
+git tag
+egrep $(tools/latest_release --no_dots --prefix=version-) README.md
+egrep $(tools/latest_release --no_dots --prefix=version-) docs/index.md
+
+# Get the templight binary
+
+cd 3rd/templight
+mkdir -p build/bin
+  cd build/bin
+    wget https://github.com/sabel83/templight_binary/releases/download/templight_35f974/templight_osx10.10.4_x86_64.tar.bz2
+    tar -xvjf templight_osx10.10.4_x86_64.tar.bz2
+  cd ../..
+cd ../..
 
 # Test the code
 
@@ -17,7 +35,10 @@ cmake .. -DCMAKE_CXX_FLAGS:STRING="-Werror" -DTEMPLIGHT_DEBUG=true
 make -j2
 make test || (cat Testing/Temporary/LastTest.log && false)
 
-# TODO set up clang binary for core STs
+for t in core mdb; do
+  test/system/app/${t}/metashell_${t}_system_test app/metashell -I../3rd/boost/include $(../tools/clang_default_path --gcc clang++ | sed 's/, "/-I/g' | sed 's/"//g' | awk '{printf("%s ", $0);}')
+
+done
 
 # Test that the documentation about the built-in pragmas and mdb commands is up to date
 
