3d447521eba3c344be99628ceb9c164819c9121d
Andras Kucsma <andras.kucsma@prezi.com>
2015-12-23 21:25:25 +0100

    Add -x to vagrant related build files

--- tools/install_system_deps.sh
+++ tools/install_system_deps.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
+set -ex
 
 if [ ! -d cmake ]
 then
