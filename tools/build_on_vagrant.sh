058add7f5b37b3ba02907a7e7b1be52ff5688436
Andras Kucsma <andras.kucsma@prezi.com>
2015-12-23 19:46:56 +0100

    Build opensuse packages using Vagrant

---
+++ tools/build_on_vagrant.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+# Metashell - Interactive C++ template metaprogramming shell
+# Copyright (C) 2015, Andras Kucsma (andras.kucsma@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+if [ ! -d cmake ]
+then
+  echo "Please run this script from the root directory of the Metashell source code"
+  exit 1
+fi
+
+if [ -z "$1" ];
+then
+  echo "Usage: $0 <platform>"
+  exit 1
+fi
+
+TARGET_PLATFORM="$1"
+
+VAGRANT_DIR="tools/vagrant/${TARGET_PLATFORM}"
+VAGRANT_FILE="${VAGRANT_DIR}/Vagrantfile"
+
+if [ ! -f "${VAGRANT_FILE}" ];
+then
+  echo "Error: ${VAGRANT_FILE} doesn't exist"
+  exit 1
+fi
+
+# VAGRANT_DIR will be mounted up to the guest to /vagrant
+# Rename it, so it's easy to ignore with .gitignore
+cp tools/full_build.sh "${VAGRANT_DIR}/ignored_full_build.sh"
+
+cd "${VAGRANT_DIR}"
+  vagrant up --provider virtualbox
+  vagrant ssh -c "/vagrant/ignored_full_build.sh"
+  vagrant halt
+cd ../../..
---
+++ tools/install_system_deps.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Metashell - Interactive C++ template metaprogramming shell
+# Copyright (C) 2015, Andras Kucsma (andras.kucsma@gmail.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+if [ ! -d cmake ]
+then
+  echo "Please run this script from the root directory of the Metashell source code"
+  exit 1
+fi
+
+PLATFORM="$(tools/detect_platform.sh)"
+
+echo "Platform: ${PLATFORM}"
+
+if [ "${PLATFORM}" = "opensuse" ]
+then
+  sudo zypper --non-interactive addrepo http://download.opensuse.org/repositories/server:irc/openSUSE_13.2/server:irc.repo
+  sudo zypper --non-interactive addrepo http://download.opensuse.org/repositories/home:namtrac:clang/openSUSE_Factory/home:namtrac:clang.repo
+  sudo zypper --non-interactive refresh
+  sudo zypper --non-interactive install cmake llvm-clang readline-devel termcap rpm-build
+else
+  echo "Installing system dependiencies on ${PLATFORM} is unsupported"
+  exit 1
+fi
