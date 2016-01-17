90fd8470993c0b8d62a9bf1340ea662fdc31038c
Abel Sinkovics <abelsoft@freemail.hu>
2016-01-17 16:17:38 +0100

    Run system tests in build.sh

--- build.sh
+++ build.sh
@@ -64,5 +64,31 @@ mkdir -p bin; cd bin
   cmake ..
   make -j${BUILD_THREADS}
   make test
+
+  if [ "${PLATFORM}" = "osx" ]
+  then
+    OPTIONAL_LIBCXX="-I$(pwd)/include/metashell/libcxx"
+  else
+    OPTIONAL_LIBCXX=""
+  fi
+
+  for t in core pp mdb; do
+    test/system/app/${t}/metashell_${t}_system_test \
+      app/metashell -- "-I$(pwd)/../3rd/boost/include" --
+  
+    test/system/app/${t}/metashell_${t}_system_test \
+      app/metashell \
+      --engine clang \
+      -- \
+      "$(pwd)/app/templight_metashell" \
+      -std=c++0x \
+      -ftemplate-depth=256 \
+      -Wfatal-errors \
+      "${OPTIONAL_LIBCXX}" \
+      "-I$(pwd)/include/metashell/templight" \
+      "-I$(pwd)/../3rd/boost/include" \
+      --
+  done
+
   cpack
 cd ..
