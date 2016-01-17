1e0c85128d98c402683f2b7f4f828cf233a475b4
Abel Sinkovics <abelsoft@freemail.hu>
2016-01-17 22:22:30 +0100

    Don't generate installer for OS X in Travis

--- tools/travis_osx.sh
+++ tools/travis_osx.sh
@@ -24,7 +24,7 @@ cd ../..
 
 # Test the code
 
-BUILD_THREADS=2 CXXFLAGS=-Werror NO_TEMPLIGHT=1 ./build.sh
+BUILD_THREADS=2 CXXFLAGS=-Werror NO_TEMPLIGHT=1 NO_INSTALLER=1 ./build.sh
 
 # Test that the documentation about the built-in pragmas and mdb commands is up to date
 
