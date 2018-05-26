e56b3e3bf22b550451e319f616ff15fa1b520090
Abel Sinkovics <abelsoft@freemail.hu>
2018-05-26 15:22:49 +0200

    Fix usage information of mdb and pdb pragmas

---
+++ lib/core/mdb_usage.cpp
@@ -0,0 +1,29 @@
+// Metashell - Interactive C++ template metaprogramming shell
+// Copyright (C) 2018, Abel Sinkovics (abel@sinkovics.hu)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <metashell/data/mdb_usage.hpp>
+
+namespace metashell
+{
+  namespace data
+  {
+    std::string mdb_usage(bool preprocessor_)
+    {
+      return std::string(preprocessor_ ? "[-profile]" : "[-full|-profile]") +
+             " [" + (preprocessor_ ? "<expression>" : "<type>") + "|-]";
+    }
+  }
+}
