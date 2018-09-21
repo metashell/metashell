cd2e2edcc123d7691b31e5328ba6f69c0715ed18
Abel Sinkovics <abelsoft@freemail.hu>
2018-09-21 16:30:15 +0200

    Use target_compile_options instead of CMAKE_CXX_FLAGS to enable/disable warnings

--- cmake/Modules/Compiler.cmake
+++ cmake/Modules/Compiler.cmake
@@ -14,30 +14,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function(disable_warnings)
+function(disable_warnings TARGET)
   if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w" PARENT_SCOPE)
+    target_compile_options(${TARGET} PRIVATE -w)
     message(STATUS "Turning off warnings for GCC")
   elseif("x${CMAKE_CXX_COMPILER_ID}" STREQUAL "xClang")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w" PARENT_SCOPE)
+    target_compile_options(${TARGET} PRIVATE -w)
     message(STATUS "Turning off warnings for Clang")
   endif()
 endfunction()
 
-function(enable_warnings)
+function(enable_warnings TARGET)
   if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-    set(
-      CMAKE_CXX_FLAGS
-      "${CMAKE_CXX_FLAGS} -Wall -Wextra"
-      PARENT_SCOPE
-    )
+    target_compile_options(${TARGET} PRIVATE -Wall -Wextra)
     message(STATUS "Turning on warnings for GCC")
   elseif("x${CMAKE_CXX_COMPILER_ID}" STREQUAL "xClang")
-    set(
-      CMAKE_CXX_FLAGS
-      "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic"
-      PARENT_SCOPE
-    )
+    target_compile_options(${TARGET} PRIVATE -Wall -Wextra -pedantic)
     message(STATUS "Turning on warnings for Clang")
   endif()
 endfunction()
