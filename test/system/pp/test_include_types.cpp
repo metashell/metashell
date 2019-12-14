f2ad0fcbd99a737204d43f8f91bc0369d0ef07dd
Abel Sinkovics <abelsoft@freemail.hu>
2019-12-14 20:51:38 +0100

    Refactor: move inclue type tests to a helper function

--- test/system/pp/test_include_types.cpp
+++ test/system/pp/test_include_types.cpp
@@ -78,245 +78,254 @@ namespace
     return engine_ != "wave" && engine_ != "msvc" && engine_ != "internal" &&
            !(uses_clang(engine_) && on_windows);
   }
+
+  void test_include_path_extensions(include_test_env& env_)
+  {
+    using data::standard_headers_allowed;
+
+    constexpr auto iquote = data::include_argument_type::iquote;
+    constexpr auto capital_i = data::include_argument_type::capital_i;
+    constexpr auto isystem = data::include_argument_type::isystem;
+    constexpr auto idirafter = data::include_argument_type::idirafter;
+
+    constexpr auto sys = data::include_type::sys;
+    constexpr auto quote = data::include_type::quote;
+
+    constexpr auto nonstandard_header =
+        include_test_env::test_filename::nonstandard_header;
+    constexpr auto standard_c_header =
+        include_test_env::test_filename::standard_c_header;
+    constexpr auto standard_cpp_header =
+        include_test_env::test_filename::standard_cpp_header;
+
+    const bool std_headers_on_include_path =
+        on_include_path(standard_c_header) ||
+        on_include_path(standard_cpp_header);
+
+    const bool can_use_c_headers_without_cpp_headers =
+        !std_headers_on_include_path &&
+        can_engine_use_c_headers_without_cpp_headers(current_real_engine());
+
+    const include_test_env::result none_found =
+        include_test_env::result::none();
+    const include_test_env::result system_found =
+        include_test_env::result::system_used();
+
+    for (data::include_type type : {sys, quote})
+    {
+      /*
+       * None
+       */
+
+      ASSERT_EQ(
+          none_found, env_.include_dir_used({}, nonstandard_header, type));
+
+      ASSERT_EQ(
+          system_found, env_.include_dir_used({}, standard_c_header, type));
+
+      ASSERT_EQ(
+          system_found, env_.include_dir_used({}, standard_cpp_header, type));
+
+      if (!std_headers_on_include_path)
+      {
+        ASSERT_EQ(
+            none_found, env_.include_dir_used({}, standard_c_header, type,
+                                              standard_headers_allowed::none));
+
+        ASSERT_EQ(
+            none_found, env_.include_dir_used({}, standard_cpp_header, type,
+                                              standard_headers_allowed::none));
+      }
+
+      if (can_use_c_headers_without_cpp_headers)
+      {
+        ASSERT_EQ(
+            none_found, env_.include_dir_used({}, standard_cpp_header, type,
+                                              standard_headers_allowed::c));
+
+        ASSERT_EQ(
+            system_found, env_.include_dir_used({}, standard_c_header, type,
+                                                standard_headers_allowed::c));
+      }
+
+      /*
+       * Single
+       */
+
+      // -I
+
+      ASSERT_EQ(capital_i,
+                env_.include_dir_used({capital_i}, nonstandard_header, type));
+
+      ASSERT_EQ(capital_i,
+                env_.include_dir_used({capital_i}, standard_c_header, type));
+
+      ASSERT_EQ(capital_i,
+                env_.include_dir_used({capital_i}, standard_cpp_header, type));
+
+      if (!std_headers_on_include_path)
+      {
+        ASSERT_EQ(capital_i,
+                  env_.include_dir_used({capital_i}, standard_c_header, type,
+                                        standard_headers_allowed::none));
+
+        ASSERT_EQ(capital_i,
+                  env_.include_dir_used({capital_i}, standard_cpp_header, type,
+                                        standard_headers_allowed::none));
+      }
+
+      if (can_use_c_headers_without_cpp_headers)
+      {
+        ASSERT_EQ(capital_i,
+                  env_.include_dir_used({capital_i}, standard_cpp_header, type,
+                                        standard_headers_allowed::c));
+
+        ASSERT_EQ(capital_i,
+                  env_.include_dir_used({capital_i}, standard_c_header, type,
+                                        standard_headers_allowed::c));
+      }
+
+      // -iquote
+
+      if (!using_msvc())
+      {
+        ASSERT_EQ(type == sys ? none_found : iquote,
+                  env_.include_dir_used({iquote}, nonstandard_header, type));
+
+        ASSERT_EQ(type == sys ? system_found : iquote,
+                  env_.include_dir_used({iquote}, standard_c_header, type));
+
+        ASSERT_EQ(type == sys ? system_found : iquote,
+                  env_.include_dir_used({iquote}, standard_cpp_header, type));
+      }
+
+      if (!using_msvc() && !std_headers_on_include_path)
+      {
+        ASSERT_EQ(type == sys ? none_found : iquote,
+                  env_.include_dir_used({iquote}, standard_c_header, type,
+                                        standard_headers_allowed::none));
+
+        ASSERT_EQ(type == sys ? none_found : iquote,
+                  env_.include_dir_used({iquote}, standard_cpp_header, type,
+                                        standard_headers_allowed::none));
+      }
+
+      if (can_use_c_headers_without_cpp_headers)
+      {
+        ASSERT_EQ(type == sys ? none_found : iquote,
+                  env_.include_dir_used({iquote}, standard_cpp_header, type,
+                                        standard_headers_allowed::c));
+
+        ASSERT_EQ(type == sys ? system_found : iquote,
+                  env_.include_dir_used({iquote}, standard_c_header, type,
+                                        standard_headers_allowed::c));
+      }
+
+      // -isystem, -S
+
+      if (!using_msvc())
+      {
+        ASSERT_EQ(isystem,
+                  env_.include_dir_used({isystem}, nonstandard_header, type));
+
+        ASSERT_EQ(
+            isystem, env_.include_dir_used({isystem}, standard_c_header, type));
+
+        ASSERT_EQ(isystem,
+                  env_.include_dir_used({isystem}, standard_cpp_header, type));
+      }
+
+      if (!using_msvc() && !std_headers_on_include_path)
+      {
+        ASSERT_EQ(
+            isystem, env_.include_dir_used({isystem}, standard_c_header, type,
+                                           standard_headers_allowed::none));
+
+        ASSERT_EQ(
+            isystem, env_.include_dir_used({isystem}, standard_cpp_header, type,
+                                           standard_headers_allowed::none));
+      }
+
+      if (can_use_c_headers_without_cpp_headers)
+      {
+        ASSERT_EQ(
+            isystem, env_.include_dir_used({isystem}, standard_cpp_header, type,
+                                           standard_headers_allowed::c));
+
+        ASSERT_EQ(
+            isystem, env_.include_dir_used({isystem}, standard_c_header, type,
+                                           standard_headers_allowed::c));
+      }
+
+      // -idirafter
+
+      if (!using_msvc())
+      {
+        ASSERT_EQ(idirafter,
+                  env_.include_dir_used({idirafter}, nonstandard_header, type));
+
+        ASSERT_EQ(system_found,
+                  env_.include_dir_used({idirafter}, standard_c_header, type));
+
+        ASSERT_EQ(system_found, env_.include_dir_used(
+                                    {idirafter}, standard_cpp_header, type));
+      }
+
+      if (!using_msvc() && !std_headers_on_include_path)
+      {
+        ASSERT_EQ(idirafter,
+                  env_.include_dir_used({idirafter}, standard_c_header, type,
+                                        standard_headers_allowed::none));
+
+        ASSERT_EQ(idirafter,
+                  env_.include_dir_used({idirafter}, standard_cpp_header, type,
+                                        standard_headers_allowed::none));
+      }
+
+      if (can_use_c_headers_without_cpp_headers)
+      {
+        ASSERT_EQ(idirafter,
+                  env_.include_dir_used({idirafter}, standard_cpp_header, type,
+                                        standard_headers_allowed::c));
+
+        ASSERT_EQ(system_found,
+                  env_.include_dir_used({idirafter}, standard_c_header, type,
+                                        standard_headers_allowed::c));
+      }
+
+      /*
+       * Pairs
+       */
+
+      if (!using_msvc())
+      {
+        ASSERT_EQ(type == sys ? capital_i : iquote,
+                  env_.include_dir_used(
+                      {capital_i, iquote}, nonstandard_header, type));
+
+        ASSERT_EQ(capital_i, env_.include_dir_used({capital_i, isystem},
+                                                   nonstandard_header, type));
+
+        ASSERT_EQ(capital_i, env_.include_dir_used({capital_i, idirafter},
+                                                   nonstandard_header, type));
+
+        ASSERT_EQ(
+            type == sys ? isystem : iquote,
+            env_.include_dir_used({iquote, isystem}, nonstandard_header, type));
+
+        ASSERT_EQ(type == sys ? idirafter : iquote,
+                  env_.include_dir_used(
+                      {iquote, idirafter}, nonstandard_header, type));
+
+        ASSERT_EQ(isystem, env_.include_dir_used(
+                               {isystem, idirafter}, nonstandard_header, type));
+      }
+    }
+  }
 }
 
 TEST(include_types, tests)
 {
-  using data::standard_headers_allowed;
-
-  constexpr auto iquote = data::include_argument_type::iquote;
-  constexpr auto capital_i = data::include_argument_type::capital_i;
-  constexpr auto isystem = data::include_argument_type::isystem;
-  constexpr auto idirafter = data::include_argument_type::idirafter;
-
-  constexpr auto sys = data::include_type::sys;
-  constexpr auto quote = data::include_type::quote;
-
-  constexpr auto nonstandard_header =
-      include_test_env::test_filename::nonstandard_header;
-  constexpr auto standard_c_header =
-      include_test_env::test_filename::standard_c_header;
-  constexpr auto standard_cpp_header =
-      include_test_env::test_filename::standard_cpp_header;
-
-  const bool std_headers_on_include_path = on_include_path(standard_c_header) ||
-                                           on_include_path(standard_cpp_header);
-
-  const bool can_use_c_headers_without_cpp_headers =
-      !std_headers_on_include_path &&
-      can_engine_use_c_headers_without_cpp_headers(current_real_engine());
-
-  const include_test_env::result none_found = include_test_env::result::none();
-  const include_test_env::result system_found =
-      include_test_env::result::system_used();
-
   include_test_env env;
-
-  for (data::include_type type : {sys, quote})
-  {
-    /*
-     * None
-     */
-
-    ASSERT_EQ(none_found, env.include_dir_used({}, nonstandard_header, type));
-
-    ASSERT_EQ(system_found, env.include_dir_used({}, standard_c_header, type));
-
-    ASSERT_EQ(
-        system_found, env.include_dir_used({}, standard_cpp_header, type));
-
-    if (!std_headers_on_include_path)
-    {
-      ASSERT_EQ(
-          none_found, env.include_dir_used({}, standard_c_header, type,
-                                           standard_headers_allowed::none));
-
-      ASSERT_EQ(
-          none_found, env.include_dir_used({}, standard_cpp_header, type,
-                                           standard_headers_allowed::none));
-    }
-
-    if (can_use_c_headers_without_cpp_headers)
-    {
-      ASSERT_EQ(none_found, env.include_dir_used({}, standard_cpp_header, type,
-                                                 standard_headers_allowed::c));
-
-      ASSERT_EQ(
-          system_found, env.include_dir_used({}, standard_c_header, type,
-                                             standard_headers_allowed::c));
-    }
-
-    /*
-     * Single
-     */
-
-    // -I
-
-    ASSERT_EQ(
-        capital_i, env.include_dir_used({capital_i}, nonstandard_header, type));
-
-    ASSERT_EQ(
-        capital_i, env.include_dir_used({capital_i}, standard_c_header, type));
-
-    ASSERT_EQ(capital_i,
-              env.include_dir_used({capital_i}, standard_cpp_header, type));
-
-    if (!std_headers_on_include_path)
-    {
-      ASSERT_EQ(
-          capital_i, env.include_dir_used({capital_i}, standard_c_header, type,
-                                          standard_headers_allowed::none));
-
-      ASSERT_EQ(capital_i,
-                env.include_dir_used({capital_i}, standard_cpp_header, type,
-                                     standard_headers_allowed::none));
-    }
-
-    if (can_use_c_headers_without_cpp_headers)
-    {
-      ASSERT_EQ(
-          capital_i, env.include_dir_used({capital_i}, standard_cpp_header,
-                                          type, standard_headers_allowed::c));
-
-      ASSERT_EQ(
-          capital_i, env.include_dir_used({capital_i}, standard_c_header, type,
-                                          standard_headers_allowed::c));
-    }
-
-    // -iquote
-
-    if (!using_msvc())
-    {
-      ASSERT_EQ(type == sys ? none_found : iquote,
-                env.include_dir_used({iquote}, nonstandard_header, type));
-
-      ASSERT_EQ(type == sys ? system_found : iquote,
-                env.include_dir_used({iquote}, standard_c_header, type));
-
-      ASSERT_EQ(type == sys ? system_found : iquote,
-                env.include_dir_used({iquote}, standard_cpp_header, type));
-    }
-
-    if (!using_msvc() && !std_headers_on_include_path)
-    {
-      ASSERT_EQ(type == sys ? none_found : iquote,
-                env.include_dir_used({iquote}, standard_c_header, type,
-                                     standard_headers_allowed::none));
-
-      ASSERT_EQ(type == sys ? none_found : iquote,
-                env.include_dir_used({iquote}, standard_cpp_header, type,
-                                     standard_headers_allowed::none));
-    }
-
-    if (can_use_c_headers_without_cpp_headers)
-    {
-      ASSERT_EQ(type == sys ? none_found : iquote,
-                env.include_dir_used({iquote}, standard_cpp_header, type,
-                                     standard_headers_allowed::c));
-
-      ASSERT_EQ(type == sys ? system_found : iquote,
-                env.include_dir_used({iquote}, standard_c_header, type,
-                                     standard_headers_allowed::c));
-    }
-
-    // -isystem, -S
-
-    if (!using_msvc())
-    {
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, nonstandard_header, type));
-
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_c_header, type));
-
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_cpp_header, type));
-    }
-
-    if (!using_msvc() && !std_headers_on_include_path)
-    {
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_c_header, type,
-                                        standard_headers_allowed::none));
-
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_cpp_header, type,
-                                        standard_headers_allowed::none));
-    }
-
-    if (can_use_c_headers_without_cpp_headers)
-    {
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_cpp_header, type,
-                                        standard_headers_allowed::c));
-
-      ASSERT_EQ(
-          isystem, env.include_dir_used({isystem}, standard_c_header, type,
-                                        standard_headers_allowed::c));
-    }
-
-    // -idirafter
-
-    if (!using_msvc())
-    {
-      ASSERT_EQ(idirafter,
-                env.include_dir_used({idirafter}, nonstandard_header, type));
-
-      ASSERT_EQ(system_found,
-                env.include_dir_used({idirafter}, standard_c_header, type));
-
-      ASSERT_EQ(system_found,
-                env.include_dir_used({idirafter}, standard_cpp_header, type));
-    }
-
-    if (!using_msvc() && !std_headers_on_include_path)
-    {
-      ASSERT_EQ(
-          idirafter, env.include_dir_used({idirafter}, standard_c_header, type,
-                                          standard_headers_allowed::none));
-
-      ASSERT_EQ(idirafter,
-                env.include_dir_used({idirafter}, standard_cpp_header, type,
-                                     standard_headers_allowed::none));
-    }
-
-    if (can_use_c_headers_without_cpp_headers)
-    {
-      ASSERT_EQ(
-          idirafter, env.include_dir_used({idirafter}, standard_cpp_header,
-                                          type, standard_headers_allowed::c));
-
-      ASSERT_EQ(system_found,
-                env.include_dir_used({idirafter}, standard_c_header, type,
-                                     standard_headers_allowed::c));
-    }
-
-    /*
-     * Pairs
-     */
-
-    if (!using_msvc())
-    {
-      ASSERT_EQ(
-          type == sys ? capital_i : iquote,
-          env.include_dir_used({capital_i, iquote}, nonstandard_header, type));
-
-      ASSERT_EQ(capital_i, env.include_dir_used(
-                               {capital_i, isystem}, nonstandard_header, type));
-
-      ASSERT_EQ(capital_i, env.include_dir_used({capital_i, idirafter},
-                                                nonstandard_header, type));
-
-      ASSERT_EQ(
-          type == sys ? isystem : iquote,
-          env.include_dir_used({iquote, isystem}, nonstandard_header, type));
-
-      ASSERT_EQ(
-          type == sys ? idirafter : iquote,
-          env.include_dir_used({iquote, idirafter}, nonstandard_header, type));
-
-      ASSERT_EQ(isystem, env.include_dir_used(
-                             {isystem, idirafter}, nonstandard_header, type));
-    }
-  }
+  test_include_path_extensions(env);
 }
